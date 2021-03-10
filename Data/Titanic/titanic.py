538f8ac133e6d7273ea864898031ccc7f9caa0cf
Alana Smith <44943819+as2247@users.noreply.github.com>
2021-03-10 11:31:32 +0000

    Added code to load Titanic data set

--- Data/Titanic/titanic.py
+++ Data/Titanic/titanic.py
@@ -1,6 +1,24 @@
+import csv
+
 records = []
 headings = []
 
 def load_data(file_path):
-    print("Loading data...")
-    
+    print("Loading data...", end="")
+
+    with open(file_path) as file:
+        csv_reader = csv.reader(file)
+        headings = next(csv_reader)
+
+        for values in csv_reader:
+            records.append(values)
+    print("Done!")
+
+def run():
+    load_data("./titanic.csv")
+    num_records = len(records)
+    print(f"Successfully Loaded {num_records} records")
+
+
+if __name__ == "__main__":
+    run()
