73ff303af7724eef8b46dceb5d45a19e9eda1e20
Alana Smith <44943819+as2247@users.noreply.github.com>
2021-03-10 13:09:30 +0000

    Added extra functions

--- Data/Titanic/titanic.py
+++ Data/Titanic/titanic.py
@@ -22,6 +22,8 @@ def display_menu():
     print("[2] Display the number of passengers that survived")
     print("[3] Display the number of passengers per gender")
     print("[4] Display the number of passengers per age group")
+    print("[5] Display the number of survivors per age group")
+    print("[6] Display the number of male, third-class survivors")
     selected_option = int(input())
     return selected_option
 
@@ -72,6 +74,47 @@ def display_passengers_per_age_group():
                 elderly += 1
     print(f"Children: {children}\nAdults: {adults}\nElderly: {elderly}")
 
+
+def display_survivors_per_age_group():
+    children = 0
+    adults = 0
+    elderly = 0
+
+    children_survived = 0
+    adults_survived = 0
+    elderly_survived = 0
+
+    for record in records:
+        survived = int(record[1])
+        if record[5] != "":
+            age = float(record[5])
+            if age < 18:
+                children += 1
+                if survived == 1:
+                    children_survived += 1
+            elif age < 65:
+                adults += 1
+                if survived == 1:
+                    adults_survived += 1
+            else:
+                elderly += 1
+                if survived == 1:
+                    elderly_survived += 1
+    print(f"Children: {children_survived}/{children}, Adults: {adults_survived}/{adults}, Elderly: {elderly_survived}/{elderly}")
+
+
+def num_male_third_class_survivors():
+    survived = 0
+    for record in records:
+        gender = record[4]
+        if gender == 'male':
+            cabin_class = float(record[2])
+            if cabin_class == 3:
+                lived = float(record[1])
+                if lived == 1:
+                    survived += 1
+    print(f"{survived} third class males survived")
+
 def run():
     load_data("./titanic.csv")
     num_records = len(records)
@@ -86,6 +129,10 @@ def run():
         display_passengers_per_gender()
     elif selected_option == 4:
         display_passengers_per_age_group()
+    elif selected_option == 5:
+        display_survivors_per_age_group()
+    elif selected_option == 6:
+        num_male_third_class_survivors()
     else:
         print("Error! Option is not recognised")
 
